5d8134f91f881a088f163a5781ad96885cf756e7
ErikPelli <erikpellizzon1@gmail.com>
2022-07-23 16:37:02 +0200

    Format errors

--- fivesim/errors.py
+++ fivesim/errors.py
@@ -1,14 +1,19 @@
 class FiveSimError(Exception):
     '''An error returned by the 5 SIM API'''
+
     def get_message(self) -> str:
         return super.message if hasattr(super, "message") else ""
 
+
 class InvalidAPIKeyError(FiveSimError):
     '''Raised when the API Key is invalid'''
+
     def __init__(self) -> None:
         super().__init__("Invalid API Key")
 
+
 class BadRequestError(FiveSimError):
     '''Raised when there is an error, with its description'''
+
     def __init__(self, description: str) -> None:
         super().__init__(description)
