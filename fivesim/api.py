01354533d9a71bffedeb0bd657a6746c152a38b5
ErikPelli <erikpellizzon1@gmail.com>
2022-07-24 17:40:01 +0200

    Support get prices api - finish guest api

--- fivesim/api.py
+++ fivesim/api.py
@@ -1,7 +1,8 @@
 from typing import Any
+from fivesim.errors import BadRequestError
 from fivesim.order import ActivationProduct, Country, HostingProduct, Language, Operator
 from fivesim.request import _APIRequest
-from fivesim.response import CountryInformation, ProductInformation, _parse_countries, _parse_products
+from fivesim.response import CountryInformation, ProductInformation, _parse_guest_countries, _parse_guest_prices, _parse_guest_products
 
 
 class UserAPI(_APIRequest):
@@ -19,8 +20,7 @@ class GuestAPI(_APIRequest):
 
         :param country: Country selection, ANY_COUNTRY is possible
         :param operator: Operator selection, ANY_OPERATOR is possible
-        :return: dictionary with the association between a Product and its information,
-                 use isinstance() to check if the Product is ActivationProduct or HostingProduct
+        :return: dictionary with the association between a Product and its information
         :raises FiveSimError: if the response is invalid
         """
         api_result = super()._GET(
@@ -29,7 +29,37 @@ class GuestAPI(_APIRequest):
         )
         return super()._parse_json(
             input=api_result.body,
-            into_object=_parse_products
+            into_object=_parse_guest_products
+        )
+
+    def get_prices(self, country: Country = None, product: ActivationProduct = None) -> dict[Country, dict[ActivationProduct, dict[Operator, ProductInformation]]]:
+        """
+        Get prices.
+        If country and product aren't provided, all the options will be returned.
+        If country is provided, only the prices of the products for this country will be returned.
+        If product is provided, there is a filter on the product.
+        If country and product are provided, a specific filter will be used.
+
+        :param country: Country selection
+        :param product: Product selection
+        :return: A dictionary that you can iterate over in a cycle or get a specific ProductInformation using [Country][Product][Operator]
+        :raises FiveSimError: if the response is invalid
+        """
+        params: dict[str, str] = dict()
+        if country != Country.ANY_COUNTRY and country is None:
+            params["country"] = country
+        if product is not None:
+            params["product"] = product
+        api_result = super()._GET(
+            use_token=False,
+            path="prices",
+            parameters=params
+        )
+        if api_result.body == "null":
+            raise BadRequestError("Product isn't available for the selected country")
+        return super()._parse_json(
+            input=api_result.body,
+            into_object=_parse_guest_prices
         )
 
     def get_notification(self, lang: Language) -> str:
@@ -63,7 +93,7 @@ class GuestAPI(_APIRequest):
         )
         return super()._parse_json(
             input=api_result.body,
-            into_object=_parse_countries
+            into_object=_parse_guest_countries
         )
 
 
--- fivesim/response.py
+++ fivesim/response.py
@@ -1,17 +1,17 @@
-from fivesim.order import ActivationProduct, Country, HostingProduct, Product
+from fivesim.order import ActivationProduct, Category, Country, HostingProduct, Operator
 from typing import Any, NamedTuple
 
 
 class ProductInformation(NamedTuple):
-    category: str
+    category: Category
     quantity: int
-    price: int
+    price: float
 
 
-def _parse_products(input: dict[str, dict[str, Any]]) -> Any:
+def _parse_guest_products(input: dict[str, dict[str, Any]]) -> Any:
     if "Category" in input:
         return ProductInformation(
-            category=input["Category"],
+            category=Category(input["Category"]),
             quantity=input["Qty"] if "Qty" in input else 0,
             price=input["Price"] if "Price" in input else 0
         )
@@ -20,13 +20,54 @@ def _parse_products(input: dict[str, dict[str, Any]]) -> Any:
                      HostingProduct, ProductInformation] = dict()
         for key, value in input.items():
             try:
-                if value.category == "activation":
+                if value.category == Category.ACTIVATION:
                     result[ActivationProduct(key)] = value
-                elif value.category == "hosting":
+                elif value.category == Category.HOSTING:
                     result[HostingProduct(key)] = value
             except:
                 pass
         return result
+
+
+def _parse_guest_prices(input: dict[str, dict[str, Any]]) -> Any:
+    if len(input) > 0:
+        if "count" in input:
+            return ProductInformation(
+                category=Category.ACTIVATION,
+                quantity=input["count"],
+                price=input["cost"]
+            )
+
+        result = dict()
+        hasProductInformation = isinstance(next(iter(input.values())), ProductInformation)
+        if not hasProductInformation:
+            # Get the first key of the first element, which is a dict
+            keyOfChildDictionary = next(iter(next(iter(input.values())).keys()))
+
+        for key, value in input.items():
+            try:
+                if hasProductInformation:
+                    result[Operator(key)] = value
+                elif isinstance(keyOfChildDictionary, Operator):
+                    try:
+                        result[Country(key)] = value
+                    except:
+                        result[ActivationProduct(key)] = value
+                elif isinstance(keyOfChildDictionary, Country):
+                    for key2, value2 in value.items():
+                        try:
+                            result[key2] = {ActivationProduct(key): value2}
+                        except:
+                            pass
+                elif isinstance(keyOfChildDictionary, ActivationProduct):
+                    result[Country(key)] = value
+                else:
+                    break
+            except:
+                pass
+        else:
+            return result
+    return input
 
 
 class CountryInformation(NamedTuple):
@@ -36,7 +77,7 @@ class CountryInformation(NamedTuple):
     ru: str
 
 
-def _parse_countries(input: dict[str, dict[str, Any]]) -> Any:
+def _parse_guest_countries(input: dict[str, dict[str, Any]]) -> Any:
     if "iso" in input:
         return CountryInformation(
             iso=next(iter(input["iso"])),
@@ -44,7 +85,7 @@ def _parse_countries(input: dict[str, dict[str, Any]]) -> Any:
             en=input["text_en"],
             ru=input["text_ru"],
         )
-    elif len(input) > 0 and isinstance(next(iter(input)), CountryInformation):
+    elif len(input) > 0 and isinstance(next(iter(input.values())), CountryInformation):
         result: dict[Country, CountryInformation] = dict()
         for key, value in input.items():
             try:
